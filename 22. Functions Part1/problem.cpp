0b82307897d0c8e6e040831b6fbeb47b52ffc1b4
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-09-24 14:08:21 +0300

    Add Functions Part1

---
+++ 22. Functions Part1/problem.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void display_my_card()
+{
+    cout << "Name: Mohamed Badwy Elsaid.\n";
+    cout << "Age: 23 Years.\n";
+    cout << "City: Mansoura.\n";
+    cout << "Country: Egypt.\n";
+}
+
+void display_stars()
+{
+    cout << "***************\n";
+    cout << "***************\n";
+    cout << "***************\n";
+    cout << "***************\n";
+}
+
+void love_programming()
+{
+    cout << "I love Programming.\n";
+    cout << "I promise to be the best developer ever.\n";
+}
+
+void display_H()
+{
+    cout << "*   *\n";
+    cout << "*   *\n";
+    cout << "*****\n";
+    cout << "*   *\n";
+    cout << "*   *\n";
+}
+
+int main()
+{
+    display_my_card();
+    display_stars();
+    love_programming();
+    display_H();
+
+    return 0;
+}

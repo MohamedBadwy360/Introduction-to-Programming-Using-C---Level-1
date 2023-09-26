668661b3fb943f7774f28aa7d2074dc06f7c8471
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-09-26 22:36:48 +0300

    Problem Solving

---
+++ 45. Problem Solving/letters_diagonal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 65; i <= 70; i++)
+    {
+        int counter = 1;
+        for (int j = 65; j <= i; j++)
+        {
+            cout << char(j) << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
